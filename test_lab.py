5817ebb7fb75c2625c91fc86e0b0b57364166131
Marco Alarcon <alarconm96@gmail.com>
2026-01-09 16:23:19 -0500

    refactor: wrap command script in main method, and add exception handling ;

--- test_lab.py
+++ test_lab.py
@@ -2,33 +2,41 @@ from netmiko import ConnectHandler
 
 # device object configuration
 cisco_device={
-    'device-type': 'cisco-xe',
+    'device_type': 'cisco_xe',
     'host': 'devnetsandboxiosxec8k.cisco.com',
     'username': 'alarconm96',
     'password': 'J--oMiV6a02RyLHQ',
     'port': 22
 }
 
-# commands I will run (like String[] array)
-commands = [
-    'show ip interfaces brief',
-    'show clock',
-    'show version | include uptime'
-]
+def main():
+    print(f'Connecting to {cisco_device["host"]}')
 
-print('Establishing SSH Connection...')
+    # commands I will run (like String[] array)
+    commands = [
+        'show ip interface brief',
+        'show clock',
+        'show version | include uptime'
+    ]
 
-# with statement handles opening/closing connection automatically
-with ConnectHandler(**cisco_device) as net_connect:
-    print('Successfully connected!')
-    print('-' * 20)
+    print('Establishing SSH Connection...')
 
-    for cmd in commands:
-        #send_command sends string cmd in array and waits for '#' prompt
-        output = net_connect.send_command(cmd)
+    try:
+        # with statement handles opening/closing connection automatically
+        with ConnectHandler(**cisco_device) as net_connect:
+            print('Connection successful!')
+            print('-' * 20)
 
-        print(f'COMMAND: {cmd}')
-        print(output)
-        print('-' * 20)
+            for cmd in commands:
+                #send_command sends string cmd in array and waits for '#' prompt
+                output = net_connect.send_command(cmd)
+                print(f'COMMAND: {cmd}')
+                print(output)
+                print('-' * 20)
+    except Exception as e:
+        print(f'Connection failed: {e}')
 
-print('Script complete. Coonnection closed')
+    print('Script complete. Coonnection closed')
+
+if __name__ == '__main__':
+    main()
