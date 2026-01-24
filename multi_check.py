89a6088c649c2a4e3f2d2cd9babf72ece7d04b0c
Marco Alarcon <alarconm96@gmail.com>
2026-01-24 14:14:25 -0500

    feat: create multi_check.py for connecting to multiple devices via text file

---
+++ multi_check.py
@@ -0,0 +1,44 @@
+from netmiko import ConnectHandler
+
+# device object configuration
+device_template = {
+    'device_type': 'cisco_xe',
+    'username': 'alarconm96',
+    'password': 'T5R2U__QZylf6',
+    'secret': 'developer'
+}
+
+# main function
+def main():
+    # using with to safely read list of devices from file
+    try:
+        with open('devices.txt', 'r') as f:
+            device_list = f.read().splitlines()
+    except FileNotFoundError:
+        print("Error: devices.txt not found.")
+        return
+    
+    # iterate through each ip in list
+    for ip in device_list:
+        print(f'\n--- Connecting to {ip} ---')
+
+        # create copy of device_template dict to use with each specific device
+        current_device = device_template.copy()
+        current_device['host'] = ip
+
+        # try-catch block and with keyword to attempt connections
+        try:
+            with ConnectHandler(**current_device) as net_connect:
+                net_connect.enable()
+                output = net_connect.send_command("show ip interface brief")
+                if "unassigned" in output:
+                    print("⚠️ WARNING: Unassigned interfaces found")
+                else:
+                    print("✅ All interfaces have IP address assigned")
+            
+        except Exception as e:
+            print(f"❌ WARNING: Connection failed wih {ip}: {e}")
+
+
+if __name__ == "__main__":
+    main()
