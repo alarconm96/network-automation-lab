eecf2b0ebc03666cf44db5f24838e6d8a7945bd8
Marco Alarcon <alarconm96@gmail.com>
2026-02-13 14:39:48 -0500

    feat: abstract multi_check.py to make use of net_utils functions for edge-router roles

--- multi_check.py
+++ multi_check.py
@@ -1,46 +1,30 @@
-from netmiko import ConnectHandler
+import getpass
+from net_utils import load_inventory, get_device_info
 
-# device object configuration
-device_template = {
-    'device_type': 'cisco_xe',
-    'username': 'alarconm96',
-    'password': 'T5R2U__QZylf6',
-    'secret': 'developer'
-}
+def run_automation():
+    # load devices from devices.yaml
+    devices = load_inventory('devices.yaml')
 
-# main function
-def main():
-    # using with to safely read list of devices from file
-    try:
-        with open('devices.txt', 'r') as f:
-            device_list = f.read().splitlines()
-    except FileNotFoundError:
-        print("Error: devices.txt not found.")
-        return
-    
-    # iterate through each ip in list
-    for ip in device_list:
-        print(f'\n--- Connecting to {ip} ---')
+    print(f"Loaded {len(devices)} total devices")
 
-        # create copy of device_template dict to use with each specific device
-        current_device = device_template.copy()
-        current_device['host'] = ip
+    # collect credentials once at the start
+    # getpass hides the text as you type it
+    username = input("Enter SSH username: ")
+    password = getpass.getpass("Enter SSH password: ")
 
-        # try-catch block and with keyword to attempt connections
-        try:
-            with ConnectHandler(**current_device) as net_connect:
-                net_connect.enable()
-                output = net_connect.send_command("show ip interface brief")
-                with open("audit_log.txt", "a") as log_file:
-                    log_file.write(f"{ip}\n")
-                    for line in output.splitlines()[2:]:
-                        if "administratively down" in line:
-                            output_string = line.split()
-                            log_file.write(f"Interface {output_string[0]} (IP {output_string[1]}) is ADMIN DOWN\n")
-                    log_file.write("-------------------------\n")
-        except Exception as e:
-            print(f"❌ WARNING: Connection failed wih {ip}: {e}")
-        print(f'--- Disconnecting from {ip} ---\n')
+    # loop - iterate through each device
+    for device in devices:
+        # only run against our routers
+        if device['role'] == 'edge-router':
+            print(f"\n--- Processing {device['hostname']} ---")
+
+            # call function from net_utils
+            result = get_device_info(device, username, password)
+
+            # print result ('show ip interface brief' output)
+            print(result)
+        else:
+            print(f"\nSkipping {device['hostname']} - not an edge router")
 
 if __name__ == "__main__":
-    main()
+    run_automation()
